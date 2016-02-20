23e4cf7922e94b01bb6bddf0822b3902f3405c89
Albo Vieira <albovieira@gmail.com>
2016-02-20 14:20:19 -0200

    ex2

---
+++ dump/init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `clientes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22 DEFAULT CHARSET=latin1;
